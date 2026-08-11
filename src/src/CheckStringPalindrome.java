95a94981ce28579a4d2a8b8e26915c53149bf9f3
Sourabhhhhh <sourabhkumbhar787571@gmail.com>
2026-08-12 00:24:40 +0530

    Check if a string is a palindrome

---
+++ src/src/CheckStringPalindrome.java
@@ -0,0 +1,26 @@
+class PalindromeString
+{
+    void checkString()
+    {
+        String str = "madam";
+
+        String reversed = new StringBuilder(str).reverse().toString();
+
+        if (str.equals(reversed))
+            {
+            System.out.println("Palindrome");
+            }
+        else
+        {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
+public class CheckStringPalindrome
+{
+    public static void main(String[] args)
+    {
+        PalindromeString obj = new PalindromeString();
+        obj.checkString();
+    }
+}
